08742693e23dd8173bd0407423fd3ce233131126
Josh Stratton <strattonbrazil@gmail.com>
2014-12-05 05:48:30 -0800

    initial build

---
+++ src/aha/routes/home.clj
@@ -0,0 +1,15 @@
+(ns aha.routes.home
+  (:require [compojure.core :refer :all]
+            [aha.layout :as layout]
+            [aha.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
